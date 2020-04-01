31525be25e43df80571aa5981c16384688c00cc7
Nico Franke <ZerNico.yt@gmail.com>
2020-04-01 22:14:11 +0200

    Use cron to delete old verification tokens

--- src/cron.ts
+++ src/cron.ts
@@ -1,8 +1,16 @@
 import { CronJob } from 'cron';
+import { getManager, LessThan, Repository } from 'typeorm';
+import { subHours } from 'date-fns';
 import { config } from './config';
+import { Token } from './entity/token';
 
-const cron = new CronJob(config.cronJobExpression, () => {
+
+const cron = new CronJob(config.cronJobExpression, async () => {
     console.log('Executing cron job once every hour');
+    // delete old verification tokens
+    const tokenRepository: Repository<Token> = getManager().getRepository(Token);
+    const token: Token[] = await tokenRepository.find({ where: { createdAt: LessThan(subHours(new Date(), 12)) } });
+    await tokenRepository.remove(token);
 });
 
 export { cron };
